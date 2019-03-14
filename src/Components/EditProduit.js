53a2a59ddcc29ce92f54cb0af544068240cc6250
tsikyFitiavana <tsikybr@gmail.com>
2019-03-14 16:41:51 +0300

    V5

--- src/Components/EditProduit.js
+++ src/Components/EditProduit.js
@@ -4,12 +4,12 @@ import './Edit.css'
 //import { confirmAlert } from "react-custom-confirm-alert"
 
 const EditProduitForm = props => {
-	const [ user, setUser ] = useState(props.currentUser)
+	const [ produit, setUser ] = useState(props.currentUser)
 
 	const handleInputChange = event => {
 		const { name, value } = event.target
 
-		setUser({ ...user, [name]: value })
+		setUser({ ...produit, [name]: value })
     }
     useEffect(
         () => {
@@ -23,22 +23,22 @@ const EditProduitForm = props => {
 			
 			onSubmit={event => {
 				event.preventDefault()
-				if(isNaN(user.username)){
+				if(isNaN(produit.prixProd)){
 					var erreur = document.getElementById('erreur')
 					erreur.innerHTML = 'Entrer un nombre'
 				}else{
-					var erreur = document.getElementById('erreur')
-					erreur.innerHTML = ''
-					props.updateUser(user.id, user)
+					var erreur2 = document.getElementById('erreur')
+					erreur2.innerHTML = ''
+					props.updateProduit(produit.id, produit)
 				}
 				
 			}}
 		> 
   
-			<input type="text" name="username" value={user.username} onChange={handleInputChange} />
+			<input type="text" name="prixProd" value={produit.prixProd} onChange={handleInputChange} />
 			<p id="erreur"></p>
 			<button className ="btn btn-secondary">OK</button>
-			&nbsp;&nbsp;<button onClick={() => props.setEditing(false)} className="btn btn-secondary">
+			&nbsp;&nbsp;<button onClick={() => props.setModifier(false)} className="btn btn-danger">
 				Annuler
 			</button>
 		</form>
